b1761533df751c5383089ca557ef4083ca75a7f3
Edinn <49732655+edinhozor@users.noreply.github.com>
2020-12-23 01:48:23 -0300

    Added name input
    Now the players can input their names, also changed some of the roll texts.

--- dice_roller.py
+++ dice_roller.py
@@ -3,16 +3,28 @@ def main():
   import random
   dice_rolls = 2
   dice_sum = 0
+
+  print(f'Enter your name:')
+  name1 = input()
+
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
-    dice_sum = dice_sum + roll
+    roll1 = random.randint(1,6)
+    dice_sum = roll + roll1
+
   if roll == 1:
-    print(f'You rolled a {roll}! Critical Fail')
+    print(f'First Roll {roll}! Critical Fail.')
   elif roll == 6:
-    print(f'You rolled a {roll}! Critical Success!')
+    print(f'First Roll {roll}! Critical Success.')
   else:
-    print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+    print(f'First Roll {roll}.')
+  if roll1 == 1:
+    print(f'Second Roll {roll1}! Critical Fail.')
+  elif roll1 == 6:
+    print(f'Second Roll {roll1}! Critical Success!')
+  else:
+    print(f'Second Roll {roll1}.')
+  print(f'{name1} have rolled a total of {dice_sum}.')
 
 
 if __name__== "__main__":
